cdc99cb5eea6230e18a7641bd479e2a9b6e9d4ce
sachin <sachinkumars1998@gmail.com>
2021-03-16 10:48:18 +0530

    second commit

---
+++ Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppEx2.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
